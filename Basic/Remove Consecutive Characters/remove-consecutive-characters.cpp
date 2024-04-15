a0429d72327a020e360be027376aa025019f02d8
Shagun Goyal <110756704+Shagun0409@users.noreply.github.com>
2024-04-15 22:52:57 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Remove Consecutive Characters/remove-consecutive-characters.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    string removeConsecutiveCharacter(string S)
+    {
+        // code here.
+      
+         string ans;
+         ans+=S[0];
+        int n=S.length();
+        for(int i=1;i<n;i++){
+            if(S[i-1]!=S[i]){
+               
+                ans+=S[i];
+               
+            }
+            
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        Solution ob;
+        cout<<ob.removeConsecutiveCharacter(s)<<endl;
+    }
+} 
+
+
+
+
+// } Driver Code Ends
