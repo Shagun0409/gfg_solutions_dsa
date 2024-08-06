c03efcfc0e7cee9b1491d592e12d7956d996581a
Shagun Goyal <110756704+Shagun0409@users.noreply.github.com>
2024-08-06 14:53:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Frequencies of Limited Range Array Elements/frequencies-of-limited-range-array-elements.cpp
+++ Difficulty: Easy/Frequencies of Limited Range Array Elements/frequencies-of-limited-range-array-elements.cpp
@@ -9,17 +9,17 @@ class Solution{
     void frequencyCount(vector<int>& arr,int N, int P)
     { 
         // code here
-        map<int,int>mp;
+        map<int,int> mp;
         for(int i=0;i<N;i++){
             mp[arr[i]]++;
         }
-       int j=0;
+        int j=0;
        for(int i=1;i<=N;i++){
            arr[j]=mp[i];
            j++;
        }
-        
-        
+       
+      
     }
 };
 
